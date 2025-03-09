03eac187ad488f93764af91dfe7c16d7fabab87f
nobodyhomeweb <nobodyhomewebsite@gmail.com>
2025-03-09 20:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
   var modal = document.getElementById("modal");
+  console.log("Modal element:", modal);
   
-  // Hide modal when the user clicks anywhere on the document
   document.addEventListener("click", function() {
+    console.log("Document clicked!");
     if (modal) {
       modal.style.display = "none";
     }
-  });
+  }, { capture: true });
 });
